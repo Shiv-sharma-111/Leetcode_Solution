aa5c8ef7de2282b4e01c483ef7b343a61b4e7600
Shiv Sharma <36760040+Shiv-sharma-111@users.noreply.github.com>
2019-11-24 13:52:23 +0530

    easy solution

---
+++ code/Plus_one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry=1;
+        for (int i=digits.size()-1; i>=0; i--)
+        {
+                digits[i]+=carry;
+                carry=digits[i]/10;
+                digits[i]=digits[i]%10;
+                if(!carry) break;
+        }
+        if (carry){
+                digits[0]=1;
+                digits.push_back(0);
+        }
+        return digits;
+    }
+};
