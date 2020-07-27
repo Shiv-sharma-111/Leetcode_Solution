4cc15066f0841590479757392393a87872dcf872
Shiv Sharma <170111019@hbtu.ac.in>
2020-07-27 02:51:50 -0400

    Create Unique-Paths.cpp

---
+++ Unique-Paths.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+
+int uniquePaths(int m, int n) 
+{
+    int widx = 0;
+    int ridx = 0;
+    
+    vector<vector<int>> mat(m+1, vector <int> (n+1, 0));
+    
+    mat[1][1] = 1;
+
+    for (int i=1; i<=m; i++)
+    {
+        for (int j=1; j<=n; j++)
+        {
+            if (i == 1 && j == 1)
+                continue;
+            mat[i][j] = mat[i-1][j] + mat[i][j-1];
+        }
+    }
+    
+    return mat[m][n];
+}
+
+};
